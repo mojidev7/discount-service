c91b4b6f6f219a138d047c05473d8e2c49b8a8c3
Mojtaba Hossein Pour <mojidev7@gmail.com>
2021-05-26 21:12:59 +0430

    fix json handler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const { router } = require('./src/service/discount/discount.route')
 const app = express()
 
+app.use(express.json())
 app.use('/api/v1', router)
 
 // app.listen(8080)
